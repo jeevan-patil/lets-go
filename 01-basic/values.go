ae590a1e99c82ce97e692d9776c3745ef474f579
Jeevan Patil <jeevanpaatil@gmail.com>
2020-04-10 17:25:37 +0800

    values additions

---
+++ 01-basic/values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is called " + "concatenation")
+	fmt.Println("1 + 3 =", (1 + 3))
+	fmt.Println("7.3/3.1 =", (7.3 / 3.1))
+	fmt.Println(true && false)
+	fmt.Println(!false)
+}
